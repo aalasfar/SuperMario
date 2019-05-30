87da3bd2c92b85e3d0603ea71dffc7926269122a
Nathaniel Aponte <aponten@uci.edu>
2019-05-29 18:37:23 -0700

    Sprites added

--- app/src/main/java/com/example/assignment4_supermario/MainActivity.java
+++ app/src/main/java/com/example/assignment4_supermario/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.assignment4_supermario;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
         setContentView(R.layout.activity_main);
+
+        Button StartGamebtn = (Button)findViewById(R.id.StartGamebtn);
+        StartGamebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(),SuperMario.class);
+                //how to pass info to second screen
+                startActivity(startIntent);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/assignment4_supermario/SuperMario.java
@@ -0,0 +1,13 @@
+package com.example.assignment4_supermario;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SuperMario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_super_mario);
+    }
+}
